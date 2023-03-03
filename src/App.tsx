09f6cd33a390306a12c47813011058d11807f32b
Mayk <maycon.lrgo@gmail.com>
2023-03-03 17:32:42 -0300

    feat: header component + toggle navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>PORTFOLIO</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
